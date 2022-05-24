07345936ad899dfcf6fa85c77fad1901cc15f9d4
Odexx <marc.vera@duocuc.cl>
2022-05-23 22:06:34 -0400

    Validaciones, apis, modal 1, get de icono, preparación segunda api

--- script.js
+++ script.js
@@ -118,3 +118,59 @@ $(document).ready(function () {
     })
 
 });
+
+/*-----------------------VALIDAR TARJETA------------------------*/
+const 
+    tarjeta = document.querySelector("#nro_tarjeta"),
+    btnAbrirFormulario = document.querySelector("#btn"),
+    formulario = document.querySelector("#formulario_tarjeta");
+    
+//VALIDAR TARJETA
+formulario.nro_tarjeta.addEventListener('keyup', (e)=>{
+    let valorInput = e.target.value;
+
+    formulario.nro_tarjeta.value = valorInput
+	// Eliminamos espacios en blanco
+	.replace(/\s/g, '')
+	// Eliminar las letras
+	.replace(/\D/g, '')
+	// Ponemos espacio cada cuatro numeros
+	.replace(/([0-9]{4})/g, '$1 ')
+	// Elimina el ultimo espaciado
+	.trim();
+
+});
+
+//VALIDAR NOMBRE
+formulario.nombre.addEventListener('keyup', (e) => {
+	debugger;
+	let valorInput = e.target.value;
+	formulario.nombre.value = valorInput.replace(/[0-9]/g, '');
+});
+
+// * Select del mes generado dinamicamente.
+for(let i = 1; i <= 12; i++){
+	let opcion = document.createElement('option');
+	opcion.value = i;
+	opcion.innerText = i;
+	formulario.mes.appendChild(opcion);
+}
+
+// * Select del año generado dinamicamente.
+const yearActual = new Date().getFullYear();
+for(let i = yearActual; i <= yearActual + 8; i++){
+	let opcion = document.createElement('option');
+	opcion.value = i;
+	opcion.innerText = i;
+	formulario.año.appendChild(opcion);
+}
+
+// VALIDAR CVV
+formulario.cvv.addEventListener('keyup', () => {
+	
+	formulario.cvv.value = formulario.cvv.value
+	// Eliminar los espacios
+	.replace(/\s/g, '')
+	// Eliminar las letras
+	.replace(/\D/g, '');
+});
