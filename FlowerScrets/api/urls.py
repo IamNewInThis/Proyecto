91d571a8f15358c3cea3b32a7f4ad8bfafc6d8b8
Nicolas <ni.ruizz@duocuc.cl>
2022-07-05 17:24:11 -0400

    Cambios con github

---
+++ FlowerScrets/api/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+
+def product_list (request):
+    pass
---
+++ FlowerScrets/core/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+#CREAR UN CONSTRUCTOR
+class persona:
+    def __init__(self, nombre,edad) -> None:
+        self.nombre = nombre
+        self.edad = edad
+
+
+# Create your views here.
+
+def root(request):
+    return redirect('/home')
+
+def home(request):
+
+    #parecido a un json - recibe un objeto, lista, string, numero
+    #actor = persona("Brad Pitt","58")
+    #lista = ["Aladin","Los Increibles","Frozen"]
+    #datos = {"nombre":"Mundo", "peliculas":lista ,"actor":actor}
+    return render (request,'core/home.html')#,datos)
+
+def maceteros(request):
+    return render(request,'core/maceteros.html')
+
+def flores(request):
+    return render(request,'core/flores.html')
+
+def subscripcion(request):
+    return render(request,'core/subscripcion.html')
+
+def tierra(request):
+    return render(request,'core/tierra.html')
+
+def arbustos(request):
+    return render(request,'core/arbustos.html')
+
+
+
